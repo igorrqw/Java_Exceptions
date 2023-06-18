1b732e8922d946834d2623a51edf8b2dfe3a1921
Василенко Игорь <warface.777@mail.ru>
2023-06-18 20:10:53 +0300

    Homework2*

--- Homework2/Task3.java
+++ Homework2/Task3.java
@@ -1,11 +1,11 @@
 package Homework2;
 
-import java.io.FileNotFoundException;
-
 /*
 Дан следующий код, исправьте его там, где требуется 
 (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 */
+
+import java.io.FileNotFoundException;
 
 public class Task3 {
     public static void main(String[] args) throws Exception {
